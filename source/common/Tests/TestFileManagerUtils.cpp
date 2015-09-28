43fd5e89fbea31838456095d5afe3bda82966a4b
gouasb_s <steven.gouasbault@epitech.eu>
2015-09-28 18:27:03 +0200

    #12: Update_Debug_QtTest

--- source/common/Tests/TestFileManagerUtils.cpp
+++ source/common/Tests/TestFileManagerUtils.cpp
@@ -1,4 +1,5 @@
 #include  <QtTest/QtTest>
+#include  <string>
 #include  "../FileManagerUtils.hpp"
 
 //
@@ -28,8 +29,16 @@ TestFileManagerUtils::~TestFileManagerUtils()
 
 void	TestFileManagerUtils::parseFileKeyValue()
 {
-    QCOMPARE("test", "test");
+	std::string fileConf("sam.conf");
+	std::vector<std::string> keyWords;
+	keyWords.push_back("port");
+	keyWords.push_back("ip");
+    	FileManagerUtils *fileManager = new FileManagerUtils;
+
+	std::vector<std::string> ret = fileManager->parseFileKeyValue(fileConf, keyWords);
+	printf("%s", keyWords.at(0).c_str());
+	QCOMPARE(keyWords.at(0), std::string("192.168.1.2;"));
 }
 
 QTEST_MAIN(TestFileManagerUtils)
-#include "testfilemanagerutils.moc"
+#include "TestFileManagerUtils.moc"
