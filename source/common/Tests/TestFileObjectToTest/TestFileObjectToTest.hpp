8b564a88ffd2c23161efeddfeadb6fb2451453dc
gouasb_s <steven.gouasbault@epitech.eu>
2015-10-01 06:57:46 +0200

    #15: update method + add Tests

---
+++ source/common/Tests/TestFileObjectToTest/TestFileObjectToTest.hpp
@@ -0,0 +1,16 @@
+#ifndef TEST_FILE_OBJECT_TO_TEST_HPP_
+# define TEST_FILE_OBJECT_TO_TEST_HPP_
+
+
+#include                                        <QtTest/QtTest>
+#include					<string>
+#include					<iostream>
+#include					<fstream>
+#include					"../../FileObject.hpp"
+
+class                                           TestFileObjectToTest
+{
+
+};
+#endif  /* !TEST_FILE_OBJECT_TO_TEST_HPP_ */
+
