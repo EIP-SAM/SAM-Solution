ffa58824b7b455d86e1dfe4652a1003cc46168c3
vuongc <christopher.k.vuong@gmail.com>
2015-11-22 17:54:32 +0800

    #27: Add non working unit test

---
+++ source/tests/TestAFunctionality/BasicAfunctionality.cpp
@@ -0,0 +1,20 @@
+#include "BasicAFunctionality.hpp"
+
+//
+// Constructor and destructor
+//
+BasicAFunctionality::BasicAFunctionality(AFunctionality::eClientId clientId, bool threaded) : AFunctionality(clientId, threaded)
+{
+}
+
+BasicAFunctionality::~BasicAFunctionality()
+{
+}
+
+//
+// Emit signal to TestAFunctionalitiesManager
+//
+void BasicAFunctionality::_run()
+{
+	emit TestRunStarted();
+}
