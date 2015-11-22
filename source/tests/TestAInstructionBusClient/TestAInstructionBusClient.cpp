9869a6e6e401facfec6eb2c9942a94781833e8d4
Nicolas Chauvin <chauvin.nico@gmail.com>
2015-11-22 18:35:25 +0530

    #28: Update TestAInstructionBusClient

--- source/tests/TestAInstructionBusClient/TestAInstructionBusClient.cpp
+++ source/tests/TestAInstructionBusClient/TestAInstructionBusClient.cpp
@@ -1,7 +1,9 @@
+#include "AInstructionBusClient.hpp"
+#include "AInstruction.hpp"
 #include <QString>
 #include <QtTest>
 
-class TestAInstructionBusClient : public QObject
+class TestAInstructionBusClient : public AInstructionBusClient
 {
     Q_OBJECT
 
@@ -9,16 +11,43 @@ public:
     TestAInstructionBusClient();
 
 private Q_SLOTS:
-    void testCase1();
+    void testClientId();
+    void testInstructionPushedSlotEnabled();
+    void testInstructionQueue();
 };
 
 TestAInstructionBusClient::TestAInstructionBusClient()
+    : AInstructionBusClient(INVALID)
 {
 }
 
-void TestAInstructionBusClient::testCase1()
+void TestAInstructionBusClient::testClientId()
 {
-    QVERIFY2(true, "Failure");
+    AInstructionBusClient client(AInstructionBusClient::INVALID);
+
+    QVERIFY(client.getClientId() == AInstructionBusClient::INVALID);
+}
+
+void TestAInstructionBusClient::testInstructionPushedSlotEnabled()
+{
+    AInstructionBusClient client(AInstructionBusClient::INVALID);
+
+    QVERIFY(client.instructionPushedSlotEnabled() == false);
+
+    client.enableInstructionPushedSlot();
+    QVERIFY(client.instructionPushedSlotEnabled() == true);
+}
+
+// `TestAInstructionBusClient` is used instead of `AInstructionBusClient`
+// to be able to access `_popInstruction()` from the unit test
+void TestAInstructionBusClient::testInstructionQueue()
+{
+    TestAInstructionBusClient client;
+    AInstruction *instructionInput = new AInstruction(), *instructionOutput = NULL;
+
+    client.pushInstruction(instructionInput);
+    instructionOutput = client._popInstruction();
+    QVERIFY(instructionInput == instructionOutput);
 }
 
 QTEST_APPLESS_MAIN(TestAInstructionBusClient)
