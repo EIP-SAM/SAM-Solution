5726941c9d6004e8fe7a8c113aee93bc0aa8892b
michau_k <alex.michaux@epitech.eu>
2015-11-22 17:04:52 +0800

    #27: Add BasicFunctionalitiesManager for unit testing purpose

---
+++ source/tests/TestAFunctionalitiesManager/BasicFunctionalitiesManager.cpp
@@ -0,0 +1,85 @@
+#include "BasicFunctionalitiesManager.hpp"
+
+//
+// Constructor
+//
+BasicFunctionalitiesManager::BasicFunctionalitiesManager(QObject *parent)
+    : AFunctionalitiesManager(parent)
+{
+}
+
+//
+// Destructor
+//
+BasicFunctionalitiesManager::~BasicFunctionalitiesManager()
+{
+}
+
+//
+// Return how many functionalities are currently running
+//
+int BasicFunctionalitiesManager::getNumberOfRunningFcts() const
+{
+    return _runningFcts.count();
+}
+
+//
+// Return how many microkernel functionalities there are
+//
+int BasicFunctionalitiesManager::getNumberOfMicrokernelFctsFcts() const
+{
+    return _microkernelFcts.count();
+}
+
+//
+// Return how many internal functionalities there are
+//
+int BasicFunctionalitiesManager::getNumberOfInternalFcts() const
+{
+    return _internalFcts.count();
+}
+
+//
+// Return how many external functionalities there are
+//
+int BasicFunctionalitiesManager::getNumberOfExternalFcts() const
+{
+    return _externalFcts.count();
+}
+
+//
+// Instanciate the various functionalities
+//
+void BasicFunctionalitiesManager::_setFcts()
+{
+    _setMicrokernelFcts();
+    _setInternalFcts();
+    _setExternalFcts();
+}
+
+//
+// Instanciate the microkernel functionalities, here 1 for the test
+//
+void BasicFunctionalitiesManager::_setMicrokernelFcts()
+{
+    _microkernelFcts << new BasicFunctionalities();
+}
+
+//
+// Instanciate the internal functionalities, here 2 for the test
+//
+void BasicFunctionalitiesManager::_setInternalFcts()
+{
+    _internalFcts << new BasicFunctionalities();
+    _internalFcts << new BasicFunctionalities();
+}
+
+//
+// Instanciate the external functionalities, here 3 for the test
+//
+void BasicFunctionalitiesManager::_setExternalFcts()
+{
+    _externalFcts << new BasicFunctionalities();
+    _internalFcts << new BasicFunctionalities();
+    _internalFcts << new BasicFunctionalities();
+}
