969cd4b831b06fd3569d7062a1a8bb5226b43c5f
vuongc <christopher.k.vuong@gmail.com>
2015-11-22 19:03:00 +0800

    #27: New TestAFunctionality.pro file for Qt5

--- source/tests/TestAFunctionality/TestAFunctionality.cpp
+++ source/tests/TestAFunctionality/TestAFunctionality.cpp
@@ -1,67 +1,107 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'TestAFunctionality.hpp'
+**
+** Created by: The Qt Meta Object Compiler version 63 (Qt 4.8.7)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
 #include "TestAFunctionality.hpp"
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'TestAFunctionality.hpp' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 63
+#error "This file was generated using the moc from 4.8.7. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
 
-//
-// Test entry function
-//
-TestAFunctionality::TestAFunctionality()
+QT_BEGIN_MOC_NAMESPACE
+static const uint qt_meta_data_TestAFunctionality[] = {
+
+ // content:
+       6,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       2,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       2,       // signalCount
+
+ // signals: signature, parameters, type, tag, flags
+      19,   27,   27,   27, 0x05,
+      28,   27,   27,   27, 0x05,
+
+       0        // eod
+};
+
+static const char qt_meta_stringdata_TestAFunctionality[] = {
+    "TestAFunctionality\0start()\0\0stop()\0"
+};
+
+void TestAFunctionality::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        Q_ASSERT(staticMetaObject.cast(_o));
+        TestAFunctionality *_t = static_cast<TestAFunctionality *>(_o);
+        switch (_id) {
+        case 0: _t->start(); break;
+        case 1: _t->stop(); break;
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
 }
 
+const QMetaObjectExtraData TestAFunctionality::staticMetaObjectExtraData = {
+    0,  qt_static_metacall 
+};
 
-//
-// Test if the QObject is threaded, return NULL if not
-//
-void TestAFunctionality::TestStart()
+const QMetaObject TestAFunctionality::staticMetaObject = {
+    { &QObject::staticMetaObject, qt_meta_stringdata_TestAFunctionality,
+      qt_meta_data_TestAFunctionality, &staticMetaObjectExtraData }
+};
+
+#ifdef Q_NO_DATA_RELOCATION
+const QMetaObject &TestAFunctionality::getStaticMetaObject() { return staticMetaObject; }
+#endif //Q_NO_DATA_RELOCATION
+
+const QMetaObject *TestAFunctionality::metaObject() const
 {
-	BasicAFunctionality *threadedFunctionality = new BasicAFunctionality((AFunctionality::eClientId)100, true);
-	QSignalSpy ThreadedStartSpy(threadedFunctionality, SIGNAL(start()));
-	QSignalSpy ThreadedRunSpy(threadedFunctionality, SIGNAL(run()));
-	threadedFunctionality->start();
-	QTest::qWait(1000);
-	QCOMPARE(ThreadedStartSpy.count(), 1);
-	QCOMPARE(ThreadedRunSpy.count(), 1);
-	QCOMPARE(threadedFunctionality->isRunning(), true);
-
-	BasicAFunctionality *nonThreadedFunctionality = new BasicAFunctionality((AFunctionality::eClientId)200, false);
-	QSignalSpy NonThreadedStartSpy(nonThreadedFunctionality, SIGNAL(start()));
-	QSignalSpy NonThreadedRunSpy(nonThreadedFunctionality, SIGNAL(run()));
-	nonThreadedFunctionality->start();
-	QTest::qWait(1000);
-	QCOMPARE(NonThreadedStartSpy.count(), 1);
-	QCOMPARE(NonThreadedRunSpy.count(), 1);
-	QCOMPARE(nonThreadedFunctionality->isRunning(), true);
-
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->metaObject : &staticMetaObject;
 }
 
-void TestAFunctionality::TestStop()
+void *TestAFunctionality::qt_metacast(const char *_clname)
 {
-	BasicAFunctionality *threadedFunctionality = new BasicAFunctionality((AFunctionality::eClientId)100, true);
-	threadedFunctionality->start();
-	QSignalSpy threadedStopSpy(threadedFunctionality, SIGNAL(stop()));
-	QTest::qWait(1000);
-	threadedFunctionality->stop();
-	QTest::qWait(1000);
-	QCOMPARE(threadedStopSpy.count(), 1);
-	QCOMPARE(threadedFunctionality->isRunning(), false);
-
-	BasicAFunctionality *nonThreadedFunctionality = new BasicAFunctionality((AFunctionality::eClientId)200, false);
-	nonThreadedFunctionality->start();
-	QSignalSpy nonThreadedStopSpy(nonThreadedFunctionality, SIGNAL(stop()));
-	QTest::qWait(1000);
-	nonThreadedFunctionality->stop();
-	QTest::qWait(1000);
-	QCOMPARE(nonThreadedStopSpy.count(), 1);
-	QCOMPARE(nonThreadedFunctionality->isRunning(), false);
-
-	BasicAFunctionality *NonStartFunctionality = new BasicAFunctionality((AFunctionality::eClientId)100, true);
-	QSignalSpy nonStartedThreadedStopSpy(NonStartFunctionality, SIGNAL(stop()));
-	QTest::qWait(1000);
-	threadedFunctionality->stop();
-	QTest::qWait(1000);
-	QCOMPARE(nonStartedThreadedStopSpy.count(), 0);
-	QCOMPARE(NonStartFunctionality->isRunning(), false);
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_TestAFunctionality))
+        return static_cast<void*>(const_cast< TestAFunctionality*>(this));
+    return QObject::qt_metacast(_clname);
 }
 
-QTEST_MAIN(TestAFunctionality)
+int TestAFunctionality::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 2)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 2;
+    }
+    return _id;
+}
 
-#include "TestAFunctionality.moc"
+// SIGNAL 0
+void TestAFunctionality::start()
+{
+    QMetaObject::activate(this, &staticMetaObject, 0, 0);
+}
+
+// SIGNAL 1
+void TestAFunctionality::stop()
+{
+    QMetaObject::activate(this, &staticMetaObject, 1, 0);
+}
+QT_END_MOC_NAMESPACE
