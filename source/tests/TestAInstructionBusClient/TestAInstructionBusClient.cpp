614a24bd8783844fa1a4545410fd3a25c860d1d0
Nicolas Chauvin <chauvin.nico@gmail.com>
2015-11-23 16:36:51 +0530

    #28: Update TestAInstructionBusClient

--- source/tests/TestAInstructionBusClient/TestAInstructionBusClient.cpp
+++ source/tests/TestAInstructionBusClient/TestAInstructionBusClient.cpp
@@ -3,6 +3,11 @@
 #include <QString>
 #include <QtTest>
 
+//
+// Note: `TestAInstructionBusClient` inherits from `AInstructionBusClient`
+//  to be able to access protected and private members
+//
+
 class TestAInstructionBusClient : public AInstructionBusClient
 {
     Q_OBJECT
@@ -16,11 +21,17 @@ private Q_SLOTS:
     void testInstructionQueue();
 };
 
+//
+// Constructor
+//
 TestAInstructionBusClient::TestAInstructionBusClient()
     : AInstructionBusClient(INVALID)
 {
 }
 
+//
+// Test instruction bus client id
+//
 void TestAInstructionBusClient::testClientId()
 {
     AInstructionBusClient client(AInstructionBusClient::INVALID);
@@ -28,6 +39,9 @@ void TestAInstructionBusClient::testClientId()
     QVERIFY(client.getClientId() == AInstructionBusClient::INVALID);
 }
 
+//
+// Test instruction pushed slot attribute
+//
 void TestAInstructionBusClient::testInstructionPushedSlotEnabled()
 {
     AInstructionBusClient client(AInstructionBusClient::INVALID);
@@ -38,13 +52,17 @@ void TestAInstructionBusClient::testInstructionPushedSlotEnabled()
     QVERIFY(client.instructionPushedSlotEnabled() == true);
 }
 
-// `TestAInstructionBusClient` is used instead of `AInstructionBusClient`
+//
+// Test instruction queue
+// Note: `TestAInstructionBusClient` is used instead of `AInstructionBusClient`
 // to be able to access `_popInstruction()` from the unit test
+//
 void TestAInstructionBusClient::testInstructionQueue()
 {
     TestAInstructionBusClient client;
     AInstruction *instructionInput = new AInstruction(), *instructionOutput = NULL;
 
+    QVERIFY(client._popInstruction() == NULL);
     client.pushInstruction(instructionInput);
     instructionOutput = client._popInstruction();
     QVERIFY(instructionInput == instructionOutput);
