f265b3c1c2eb2fb5a69acc543dc21a3c43a45a85
Christopher Vuong <christophervuong@Christophers-MacBook-Pro.local>
2015-11-25 18:23:40 +0100

    #27: Add BasicMainController class

---
+++ source/tests/TestAFunctionalitiesManager/BasicMainController.hpp
@@ -0,0 +1,36 @@
+#ifndef BASIC_MAIN_CONTROLLER_HPP
+# define BASIC_MAIN_CONTROLLER_HPP
+
+# include "AFunctionalitiesManager.hpp"
+# include "InstructionBus.hpp"
+# include "eProgId.hpp"
+
+class BasicMainController : public QObject
+{
+    Q_OBJECT
+    
+private:
+    BasicFunctionalitiesManager _basicFctsManager;
+    InstructionBus _instructionBus;
+    const eProgId _progId;
+
+protected:
+    AMainController(eProgId);
+
+public:
+    ~AMainController();
+
+    int run();
+
+protected:
+    bool _init();
+
+public:
+    bool registerClient(AInstructionBusClient::eClientId, AInstructionBusClient *);
+    bool pushInstruction(AInstruction *);
+
+    eProgId getProgId() const;
+    AFunctionality::eType getFunctionalityType(AFunctionality::eClientId) const;
+}
+
+#endif // !BASIC_MAIN_CONTROLLER_HPP
